7446fd00ee92f3d20f6db011a99520914a38d162
Маздайщик <a.v.konovalov87@mail.ru>
2020-11-27 11:35:41 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc < 2) {
+    fprintf(stderr, "%s: bad command line\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Hello, %s!\n", argv[1]);
+  return EXIT_SUCCESS;
+}
